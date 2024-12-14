5fd5238180087bbabfd6da47ff1aba7192ed6f51
Eduardo Castro <55259138+edusilva-dev@users.noreply.github.com>
2024-12-14 14:03:48 +0000

    finish `/api/v1/status` endpoint`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,43 @@
 import database from "infra/database.js";
 
 export default async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "São acima da média" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const [
+    {
+      value: {
+        rows: [{ server_version: postgresVersion }],
+      },
+    },
+    {
+      value: {
+        rows: [{ max_connections: maxConnections }],
+      },
+    },
+    {
+      value: {
+        rows: [{ count: openedConnections }],
+      },
+    },
+  ] = await Promise.allSettled([
+    await database.query("SHOW server_version;"),
+    await database.query("SHOW MAX_CONNECTIONS;"),
+    await database.query({
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+      values: [databaseName],
+    }),
+  ]);
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: postgresVersion,
+        max_connections: parseInt(maxConnections),
+        opened_connections: openedConnections,
+      },
+    },
+  });
 }
